0549696f4582b07636a3076aaee7559ec2c6c9e5
vn524h1 <ramon.parra@walmart.com>
2021-10-22 10:38:16 -0500

    Fix BcdBinaryTimeMapper

--- app/src/main/java/com/ramoncinp/relojcinbinariocompose/data/mappers/BcdBinaryTimeMapper.kt
+++ app/src/main/java/com/ramoncinp/relojcinbinariocompose/data/mappers/BcdBinaryTimeMapper.kt
@@ -3,43 +3,20 @@ package com.ramoncinp.relojcinbinariocompose.data.mappers
 import com.ramoncinp.relojcinbinariocompose.data.BcdBinaryTime
 import com.ramoncinp.relojcinbinariocompose.domain.toBinaryNibbleString
 
-const val TIME_ELEMENT_LENGTH = 2
-const val HOUR_INDEX = 0
-const val MINUTE_INDEX = 2
-const val SECOND_INDEX = 4
-
 class BcdBinaryTimeMapper {
 
     /**
      * input: Date formated as "HHmmss" like "153032" for 15:30:32
      */
     fun fromFormattedTime(formattedDate: String): BcdBinaryTime {
-        val hour = getTimeElementFromString(formattedDate, HOUR_INDEX)
-        val minutes = getTimeElementFromString(formattedDate, MINUTE_INDEX)
-        val seconds = getTimeElementFromString(formattedDate, SECOND_INDEX)
-
-        val hourPair = getTensAndUnits(hour)
-        val minutesPair = getTensAndUnits(minutes)
-        val secondsPair = getTensAndUnits(seconds)
-
-        val listOfResults = mutableListOf<Int>()
-        listOfResults.addAll(hourPair.toList())
-        listOfResults.addAll(minutesPair.toList())
-        listOfResults.addAll(secondsPair.toList())
+        val listOfResults = formattedDate.toList().map {
+            it.toString().toInt()
+        }
 
         val binaryResults = listOfResults.map { intResult ->
             intResult.toBinaryNibbleString()
         }
 
         return BcdBinaryTime(binaryResults)
-    }
-
-    private fun getTimeElementFromString(formattedDate: String, startIndex: Int) =
-        formattedDate.substring(startIndex, startIndex + TIME_ELEMENT_LENGTH).toInt()
-
-    private fun getTensAndUnits(intValue: Int): Pair<Int, Int> {
-        val tensVal = intValue / 10
-        val unitsVal = intValue % 10
-        return Pair(tensVal, unitsVal)
     }
 }
